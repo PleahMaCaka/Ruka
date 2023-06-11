d7a39c81617e6477288d5400351b27227556933d
pleahmacaka <pleahmacaka@gmail.com>
2023-06-11 23:11:23 +0900

    chore: icons better

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
   import AddressBar from "./lib/components/AddressBar.svelte";
+  import SideBar from "./lib/components/SideBar.svelte";
 </script>
 
 <main class="container">
   <AddressBar/>
+  <SideBar>
+
+  </SideBar>
 </main>
 
 <style>
-  #title {
-    color: #747bff;
-    display: inline-block;
-  }
+  /*.logo.vite:hover {*/
+  /*  filter: drop-shadow(0 0 2em #747bff);*/
+  /*}*/
 
-  #title2 {
-    display: inline-block;
-  }
-
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
+  /*.logo.svelte:hover {*/
+  /*  filter: drop-shadow(0 0 2em #ff3e00);*/
+  /*}*/
 </style>
--- src/lib/components/AddressBar.svelte
+++ src/lib/components/AddressBar.svelte
@@ -4,45 +4,51 @@
 
 <main class="address-container">
   <div class="icon-container">
-    <div class="icon-box">
-      <img src="./address-bar/home.svg" alt="Home"/>
-    </div>
+    <img class="button-icon" alt="Left" src="./address-bar/arrow-left.svg"/>
+    <img class="button-icon" alt="Right" src="./address-bar/arrow-right.svg"/>
+    <img class="button-icon" alt="Home" src="./address-bar/home.svg"/>
   </div>
   <input class="address-input">
 </main>
 
 <style>
+  .button-icon {
+    margin-left: 4px;
+  }
+
+  .icon-container {
+    display: flex;
+    justify-content: flex-start;
+    margin-right: 8px
+  }
+
   .address-input {
     width: 100%;
     height: 32px;
     border: none;
     border-radius: 5px;
-    background-color: rgba(230, 230, 230, 0.2);
+    background-color: rgba(230, 230, 230, 0.05);
     color: white;
     font-size: 16px;
     padding: 0 10px;
     outline: none;
+    margin-right: 12px;
   }
 
-  .icon-box {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    width: 32px;
-    height: 32px;
-    border-radius: 50%;
-    margin-right: 10px;
-  }
-
-  .icon-box:hover {
-    transition: 0.13s;
-    background-color: rgba(230, 230, 230, 0.2);
+  .address-input:hover, .address-input:focus {
+    transition: 0.2s;
+    background-color: rgba(230, 230, 230, 0.1);
   }
 
   .address-container {
     display: flex;
     flex-direction: row;
     justify-content: center;
+
+    align-items: center;
+    width: 100%;
+    height: 32px;
+    border-radius: 50%;
+    margin-right: 10px;
   }
 </style>
