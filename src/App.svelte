efb7c50a1fddc850d2bf80e939c57209a6eaf098
pleahmacaka <pleahmacaka@gmail.com>
2023-06-11 18:13:24 +0900

    :star:

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import AddressBar from "./lib/components/AddressBar.svelte";
+</script>
+
+<main class="container">
+  <AddressBar/>
+</main>
+
+<style>
+  #title {
+    color: #747bff;
+    display: inline-block;
+  }
+
+  #title2 {
+    display: inline-block;
+  }
+
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
+
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00);
+  }
+</style>
---
+++ src/lib/components/AddressBar.svelte
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+
+<main class="address-container">
+  <div class="icon-container">
+    <div class="icon-box">
+      <img src="./address-bar/home.svg" alt="Home"/>
+    </div>
+  </div>
+  <input class="address-input">
+</main>
+
+<style>
+  .address-input {
+    width: 100%;
+    height: 32px;
+    border: none;
+    border-radius: 5px;
+    background-color: rgba(230, 230, 230, 0.2);
+    color: white;
+    font-size: 16px;
+    padding: 0 10px;
+    outline: none;
+  }
+
+  .icon-box {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  .icon-box:hover {
+    transition: 0.13s;
+    background-color: rgba(230, 230, 230, 0.2);
+  }
+
+  .address-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+</style>
