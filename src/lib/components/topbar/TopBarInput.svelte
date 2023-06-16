b1437c5ed23ad3a7d3dae47952597640741a442b
pleahmacaka <pleahmacaka@gmail.com>
2023-06-16 23:10:47 +0900

    add top-bar input padding

--- src/lib/components/topbar/TopBarInput.svelte
+++ src/lib/components/topbar/TopBarInput.svelte
@@ -1,20 +1,23 @@
 <script>
+  let currentPath = ""
 </script>
 
 <main class="address-container">
-  <input class="address-input">
+  <input class="address-input" bind:value={currentPath} on:input={() => console.log(currentPath)}>
 </main>
 
 <style>
   .address-input {
     width: 72.3vw;
     height: 24px;
-    border: none;
+    border: rgba(169, 169, 169, 0.1) 1px solid;
     border-radius: 5px;
     background-color: rgba(230, 230, 230, 0.05);
+
     color: white;
-    font-size: 16px;
+    font-size: 14px;
     outline: none;
+    padding: 0 12px;
   }
 
   .address-input:hover, .address-input:focus {
